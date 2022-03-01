d2d2e8659dab168f621574babd7f3dbcd4f409a6
13emaals <100694169+13emaals@users.noreply.github.com>
2022-03-01 13:49:50 +0100

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,40 @@
+basic.showLeds(`
+    # # # # #
+    # . . . .
+    # # # . .
+    # . . . .
+    # # # # #
+    `)
+music.playMelody("E D E D E D E D ", 500)
+basic.showLeds(`
+    # . . . #
+    # # . # #
+    # . # . #
+    # . . . #
+    # . . . #
+    `)
+music.playMelody("C D C D C D C D ", 500)
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
+music.playMelody("E D E D E D E D ", 500)
+basic.showLeds(`
+    # # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # .
+    `)
+music.playMelody("A G A G A G A G ", 500)
+basic.showLeds(`
+    # # . # #
+    # # . # #
+    . . . . .
+    # # # # #
+    . # # # .
+    `)
+music.playMelody("D D D D D D D D ", 120)
